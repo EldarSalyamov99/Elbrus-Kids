883e9d930be4dd271f5aea7958dec207396a8e23
Vazgen <Kirakosyan_Vazgen@mail.ru>
2023-09-21 20:10:31 +0300

    Форма для регистрации

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './Components/Pages/MainPage';
 import PersonalPage from './Components/Pages/PersonalPage';
+import SignUp from './Components/Pages/SignUp';
 
 function App(): JSX.Element {
 
@@ -9,6 +10,7 @@ function App(): JSX.Element {
     <Routes>
       <Route path='/' element={<MainPage/>} />
       <Route path='/personal' element={<PersonalPage/>} />
+      <Route path='/signup' element={<SignUp/>} />
     </Routes>
   );
 }
--- client/src/Components/Pages/MainPage.tsx
+++ client/src/Components/Pages/MainPage.tsx
@@ -35,7 +35,7 @@ export default function MainPage(): JSX.Element {
             </li>
           ))}
         </ul>
-        <div className='bg-indigo-'>ggg</div>
+        <div className='bg-emerald-900'>ggg</div>
       </div>
     </div>
   )

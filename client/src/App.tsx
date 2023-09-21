f2eec52b06474c0d4883a2336ac03bd5a108cd3f
LizaVe <elize.ve21@gmail.com>
2023-09-21 20:15:12 +0300

    SingUp Page Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './Components/Pages/MainPage';
 import PersonalPage from './Components/Pages/PersonalPage';
+import SignIn from './Components/Pages/SignIn';
 
 function App(): JSX.Element {
 
@@ -9,6 +10,7 @@ function App(): JSX.Element {
     <Routes>
       <Route path='/' element={<MainPage/>} />
       <Route path='/personal' element={<PersonalPage/>} />
+      <Route path='/signin' element={<SignIn/>} />
     </Routes>
   );
 }

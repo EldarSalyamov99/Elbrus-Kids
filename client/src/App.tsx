48c9dbef3f4454f4a154550195dbc9069d3bdab0
EldarSalyamov99 <eldarsalamov99@gmail.com>
2023-09-22 10:21:43 +0300

    Тест Навигация

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './Components/Pages/MainPage';
 import PersonalPage from './Components/Pages/PersonalPage';
+import LeftBar from './Components/UI/LeftBar';
+import RightBar from './Components/UI/RightBar';
 
 function App(): JSX.Element {
 
   return (
+    <>
+    <LeftBar/>
     <Routes>
       <Route path='/' element={<MainPage/>} />
       <Route path='/personal' element={<PersonalPage/>} />
     </Routes>
+    <RightBar/>
+    </>
   );
 }
 
--- client/src/Components/UI/NavBar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NavBar():JSX.Element {
-  return (
-    <div>NavBar</div>
-  )
-}

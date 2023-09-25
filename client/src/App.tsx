a57329a842341c454e879856cf3ed9793f2c1d57
Vazgen <Kirakosyan_Vazgen@mail.ru>
2023-09-25 18:43:23 +0300

    modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './Components/Pages/MainPage';
 import PersonalPage from './Components/Pages/PersonalPage';
-import LeftBar from './Components/UI/LeftBar';
-import RightBar from './Components/UI/RightBar';
 import SignIn from './Components/Pages/SignIn';
 import SignUp from './Components/Pages/SignUp';
 import PrivateRoute from './Components/PrivateRoute';
@@ -12,8 +10,7 @@ import { useAppSelector } from './Features/Redux/hooks/reduxHooks';
 function App(): JSX.Element {
   const user = useAppSelector((state) => state.user);
   return (
-    <>
-      <LeftBar />
+    
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/personal" element={<PersonalPage />} />
@@ -22,8 +19,7 @@ function App(): JSX.Element {
           <Route path="/signup" element={<SignUp />} />
         </Route>
       </Routes>
-      <RightBar />
-    </>
+  
   );
 }
 

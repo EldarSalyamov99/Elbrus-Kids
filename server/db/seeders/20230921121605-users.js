d58cd4685591f2ec41a6f452b4d5a7ff05db5e7d
EldarSalyamov99 <eldarsalamov99@gmail.com>
2023-09-26 10:37:38 +0300

    s

--- server/db/seeders/20230921121605-users.js
+++ server/db/seeders/20230921121605-users.js
@@ -1,59 +1,61 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Categories', [
-      {
-        catName: 'HTML',
-        themeId: null,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        catName: 'Введение в HTML, списки и гиперссылки',
-        themeId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        catName: 'Таблицы и основы CSS',
-        themeId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        catName: 'Псевдоэлементы',
-        themeId: 4,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        catName: 'Flex',
-        themeId: 4,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        catName: 'JavaScript',
-        themeId: null,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        catName: 'Классы',
-        themeId: 7,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        catName: 'Методы массивов',
-        themeId: 7,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ],
-    {},
-  );
-  
+    await queryInterface.bulkInsert(
+      'Categories',
+      [
+        {
+          catName: 'HTML',
+          themeId: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          catName: 'Введение в HTML, списки и гиперссылки',
+          themeId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          catName: 'Таблицы и основы CSS',
+          themeId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          catName: 'Псевдоэлементы',
+          themeId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          catName: 'Flex',
+          themeId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          catName: 'JavaScript',
+          themeId: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          catName: 'Классы',
+          themeId: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          catName: 'Методы массивов',
+          themeId: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+
     await queryInterface.bulkInsert('Questions', [
       {
         questions: 'Какой HTML-тег используется для создания таблицы?',
@@ -78,7 +80,7 @@ module.exports = {
       },
       {
         questions: 'Какой CSS-свойство используется для задания ширины границы ячейки таблицы?',
-         catId: 3,
+        catId: 3,
         createdAt: new Date(),
         updatedAt: new Date(),
         video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
@@ -92,7 +94,7 @@ module.exports = {
       },
       {
         questions: 'Какая единица измерения используется для задания высоты строки таблицы?',
-         catId: 3,
+        catId: 3,
         createdAt: new Date(),
         updatedAt: new Date(),
         video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
@@ -106,26 +108,28 @@ module.exports = {
       },
       {
         questions: 'Какой HTML-тег используется для создания заголовка таблицы?',
-         catId: 3,
+        catId: 3,
         createdAt: new Date(),
         updatedAt: new Date(),
         video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
       },
       {
         questions: 'Какой CSS-свойство используется для задания отступов внутри ячейки таблицы?',
-         catId: 3,
+        catId: 3,
         createdAt: new Date(),
         updatedAt: new Date(),
         video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
       },
       {
         questions: 'Какой атрибут используется для задания границы таблицы?',
-         catId: 3,
+        catId: 3,
         createdAt: new Date(),
         updatedAt: new Date(),
         video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
-      }, 
-      await queryInterface.bulkInsert('Questions', [
+      }], {});
+    await queryInterface.bulkInsert(
+      'Questions',
+      [
         {
           questions: 'Какой HTML-тег используется для создания таблицы?',
           catId: 3,
@@ -149,7 +153,7 @@ module.exports = {
         },
         {
           questions: 'Какой CSS-свойство используется для задания ширины границы ячейки таблицы?',
-           catId: 3,
+          catId: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
           video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
@@ -163,7 +167,7 @@ module.exports = {
         },
         {
           questions: 'Какая единица измерения используется для задания высоты строки таблицы?',
-           catId: 3,
+          catId: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
           video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
@@ -177,100 +181,99 @@ module.exports = {
         },
         {
           questions: 'Какой HTML-тег используется для создания заголовка таблицы?',
-           catId: 3,
+          catId: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
           video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
         },
         {
           questions: 'Какой CSS-свойство используется для задания отступов внутри ячейки таблицы?',
-           catId: 3,
+          catId: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
           video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
         },
         {
           questions: 'Какой атрибут используется для задания границы таблицы?',
-           catId: 3,
+          catId: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
           video: 'https://www.youtube.com/watch?v=8bp5nCL7Uu0',
-        }, 
-           {
-            questions: 'Какое расширение имеют файлы HTML?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для создания заголовка первого уровня?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для создания ссылки?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-              video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для создания списка с маркированными пунктами?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-              video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для создания таблицы?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-              video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для вставки изображения?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-              video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для создания абзаца текста?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-              video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для выделения жирного текста?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-              video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для создания формы ввода текста?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-              video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-           },
-           {
-            questions: 'Какой тег используется для создания кнопки?',
-            catId: 2,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-              video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
-            },
-          ],
-          {},
-        );
-        
-        
+        },
+        {
+          questions: 'Какое расширение имеют файлы HTML?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для создания заголовка первого уровня?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для создания ссылки?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для создания списка с маркированными пунктами?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для создания таблицы?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для вставки изображения?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для создания абзаца текста?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для выделения жирного текста?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для создания формы ввода текста?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+        {
+          questions: 'Какой тег используется для создания кнопки?',
+          catId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          video: 'https://www.youtube.com/watch?v=lEt1JS0e8h4&ab_channel=%D0%9F%D0%B8%D0%BA%D1%81%D0%B5%D0%BB%D1%8C-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D0%B4%D0%BB%D1%8F%D0%B4%D0%B5%D1%82%D0%B5%D0%B9',
+        },
+      ],
+      {},
+    );
+
     await queryInterface.bulkInsert(
       'Answers',
       [
